8b11a6b12894b7eceef2f747bbe8b01d531c23bc
Ranlot <ranlot75@gmail.com>
2016-04-11 11:27:06 +0200

    sanity check

--- clusterMaker.R
+++ clusterMaker.R
@@ -6,6 +6,7 @@ library(parallel)
 
 #custom zip function...
 zipper <- function(xsDescribe, xs, ysDescribe, ys) {
+  stopifnot(length(xs) == length(ys))
   zippedArgs <- mapply(c, xs, ys)
   row.names(zippedArgs) <- c(xsDescribe, ysDescribe)
   do.call(c, apply(zippedArgs, 2, list))
